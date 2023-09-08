358a2a91e4f4fda918667380610f8779ab02e2d6
nijatabdull <nijattttsa@gmail.com>
2023-09-08 16:18:52 +0400

    Add array types and reset git cache

--- Algorithm&DataStructures/Algorithm&DataStructures.Initial/Program.cs
+++ Algorithm&DataStructures/Algorithm&DataStructures.Initial/Program.cs
@@ -4,9 +4,33 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Array array = Array.CreateInstance(typeof(int), 10);
+
+            array.SetValue(1, 0);
 
             Console.ReadLine();
+        }
+
+        static void MultiDimensionArray()
+        {
+            int[,] arr = new int[2, 1] { { 1 }, { 1 } };
+
+
+        }
+
+        static void JaggedArray()
+        {
+            int[][] arr = new int[4][];
+
+            arr[0] = new int[1];
+            arr[1] = new int[2];
+            arr[2] = new int[3];
+            arr[3] = new int[4];
+
+            //1
+            //2 3
+            //4 5 6
+            //7 8 9 10
         }
     }
 }
