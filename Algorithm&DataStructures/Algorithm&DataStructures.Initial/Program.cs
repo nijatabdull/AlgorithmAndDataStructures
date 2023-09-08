28cbd902468766600d54cda4b6a2ebc1883accbb
nijatabdull <nijattttsa@gmail.com>
2023-09-08 09:11:58 +0400

    Initial commit

---
+++ Algorithm&DataStructures/Algorithm&DataStructures.Initial/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Algorithm_DataStructures.Initial
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
