f69b01a42bd5ab6c7c78fa4c8093497f69c3f1cf
Abdullayev Nijat <NiAbdullayev@pasha-insurance.az>
2023-09-13 18:46:57 +0400

    Add selection sort algorithm

---
+++ Algorithm&DataStructures/Algorithm.SelectionSort/Program.cs
@@ -0,0 +1,47 @@
+﻿namespace Algorithm.SelectionSort
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] ints = { 11, 2, 3, 1, 6, -1, 4, 2, 7 };
+
+            int[] sorted = SelectionSort(ints);
+
+            foreach (int i in sorted)
+            {
+                Console.WriteLine(i);
+            }
+
+            Console.ReadKey();
+        }
+
+        public static int[] SelectionSort(int[] arr)
+        {
+            // 11, 2, 3, 1, 6
+            // 1, 2, 3, 6, 11
+            // 1, 2, 3, 6, 11
+
+            for (int i = arr.Length - 1; i >= 0; i--)
+            {
+                int selected = 0;
+                for (int j = 0; j < i; j++)
+                {
+                    if (arr[selected] < arr[j + 1])
+                    {
+                        selected = j + 1;
+                    }
+                }
+
+                if (arr[i] < arr[selected])
+                {
+                    int temp = arr[selected];
+                    arr[selected] = arr[i];
+                    arr[i] = temp;
+                }
+            }
+
+            return arr;
+        }
+    }
+}
