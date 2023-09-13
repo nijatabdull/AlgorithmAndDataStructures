6e06679f8c43c71077e6bde0525d2b6853de4218
Abdullayev Nijat <NiAbdullayev@pasha-insurance.az>
2023-09-13 08:51:34 +0400

    Bubble sort project was added

---
+++ Algorithm&DataStructures/Algorithm.BubbleSort/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace Algorithm.BubbleSort
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] ints = { 11, 2, 3, 1, 6 };
+
+            int[] sorted = BubbleSort(ints);
+
+            foreach (int i in sorted)
+            {
+                Console.WriteLine(i);
+            }
+
+            Console.ReadKey();
+        }
+
+        internal static int[] BubbleSort(int[] nums)
+        {
+            return nums;
+        }
+    }
+}
