6588e2396b5a5869d3572b3801480f8a4c707b9f
Abdullayev Nijat <NiAbdullayev@pasha-insurance.az>
2023-09-13 18:08:28 +0400

    Add bubble sort algorithm

--- Algorithm&DataStructures/Algorithm.BubbleSort/Program.cs
+++ Algorithm&DataStructures/Algorithm.BubbleSort/Program.cs
@@ -18,6 +18,25 @@
 
         internal static int[] BubbleSort(int[] nums)
         {
+            // 11, 2, 3, 1, 6
+            // 2, 11, 3, 1, 6
+            // 2, 3, 11, 1, 6
+            // 2, 3, 1, 11, 6
+            // 2, 3, 1, 6, 11
+
+            for (int i = nums.Length -1; i >= 0; i--)
+            {
+                for(int j = 0; j < i; j++)
+                {
+                    if (nums[j] > nums[j + 1])
+                    {
+                        int temp = nums[j];
+                        nums[j] = nums[j + 1];
+                        nums[j + 1] = temp;
+                    }
+                }
+            }
+
             return nums;
         }
     }
