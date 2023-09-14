fd4716a006ba646266ee248f77fef6e507435656
Abdullayev Nijat <NiAbdullayev@pasha-insurance.az>
2023-09-14 18:49:04 +0400

    Add recursive algorithm example

---
+++ Algorithm&DataStructures/Algorithm.Recursion/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace Algorithm.Recursion
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int result = Factorial(100000);
+
+            Console.WriteLine(result);
+
+            Console.ReadKey();
+        }
+
+        internal static int Factorial(int x)
+        {
+            // n! = n * (n -1)!
+
+            // 5! = 1 * 2 * 3 * 4 * 5
+            // 1 * 2 * 3 * 20
+
+            if (x < 0)
+                throw new InvalidOperationException("Value can not be negative.");
+
+            if (x == 0)
+                return 1;
+
+            return Factorial(x - 1) * x;
+        }
+    }
+}
