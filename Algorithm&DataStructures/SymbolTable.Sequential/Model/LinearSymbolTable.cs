6f7936b38766456e9e44ae8e6a8791738bab24b7
AbdullayevN <NiAbdullayev@pasha-insurance.az>
2023-10-16 09:10:12 +0400

    Add sequential symbol table implementation

---
+++ Algorithm&DataStructures/SymbolTable.Sequential/Model/LinearSymbolTable.cs
@@ -0,0 +1,142 @@
+﻿namespace SymbolTable.Sequential.Model
+{
+    public class LinearSymbolTable<TKey, TValue>
+    {
+        private class Node
+        {
+            public TKey Key { get; }
+            public TValue Value { get; }
+
+            public Node Next { get; set; }
+
+            public Node(TKey key, TValue value, Node next)
+            {
+                Key = key;
+                Value = value;
+                Next = next;
+            }
+        }
+
+        private IEqualityComparer<TKey> _comparer;
+
+        public LinearSymbolTable()
+        {
+            _comparer = EqualityComparer<TKey>.Default;
+        }
+
+        public LinearSymbolTable(IEqualityComparer<TKey> equalityComparer)
+        {
+            _comparer = equalityComparer ?? throw new ArgumentNullException(nameof(equalityComparer));
+        }
+
+        private Node _head;
+
+        private int _size = 0;
+        private int _version = 0;
+
+        public int Count => _size;
+        public bool IsEmpty => _size == 0;
+
+        public void Add(TKey key, TValue value)
+        {
+            if (key == null) throw new ArgumentNullException(nameof(key));
+
+            if (_head == null)
+            {
+                _head = new Node(key, value, default);
+            }
+            else
+            {
+                Node node = _head;
+
+                while (node != null)
+                {
+                    if (_comparer.Equals(key, node.Key)) throw new InvalidOperationException();
+
+                    node = node.Next;
+                }
+
+                _head = new Node(key, value, _head);
+            }
+
+            _size++;
+            _version++;
+        }
+
+        public bool Remove(TKey key)
+        {
+            if (key == null) throw new ArgumentNullException(nameof(key));
+
+            if (_head == null) throw new ArgumentNullException("List is emptry");
+
+            Node node = _head;
+            Node previous = default;
+
+            while (node != null)
+            {
+                if (_comparer.Equals(key, node.Key))
+                {
+                    if (previous == null)
+                        _head = default;
+                    else
+                        previous.Next = node.Next;
+
+                    _size--;
+                    _version++;
+
+                    return true;
+                }
+
+                previous = node;
+                node = node.Next;
+            }
+
+            return false;
+        }
+
+        public void TryGet(TKey key, out TValue value)
+        {
+            if (key == null) throw new ArgumentNullException(nameof(key));
+
+            if (_head == null) throw new ArgumentNullException("List is emptry");
+
+            if (_head == null)
+            {
+                value = _head.Value; return;
+            }
+            else
+            {
+                Node node = _head;
+
+                while (node != null)
+                {
+                    if (_comparer.Equals(key, node.Key))
+                    {
+                        value = node.Value;
+
+                        return;
+                    }
+
+                    node = node.Next;
+                }
+            }
+
+            value = default;
+        }
+
+        public IEnumerable<TKey> Keys()
+        {
+            Node node = _head;
+
+            int version = _version;
+
+            while (node != null)
+            {
+                if (_version != version)
+                    throw new InvalidOperationException();
+
+                yield return node.Key;
+            }
+        }
+    }
+}
---
+++ Algorithm&DataStructures/SymbolTable.Sequential/Program.cs
@@ -0,0 +1,26 @@
+﻿using SymbolTable.Sequential.Model;
+
+namespace SymbolTable.Sequential
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            LinearSymbolTable<int, string> linearSymbolTable = new LinearSymbolTable<int, string>();
+
+            linearSymbolTable.Add(1, "Nicat");
+            linearSymbolTable.Add(2, "Ruslan");
+            linearSymbolTable.Add(3, "Tofig");
+            linearSymbolTable.Add(4, "Hesen");
+            linearSymbolTable.Add(5, "Yusif");
+
+            bool del = linearSymbolTable.Remove(3);
+
+            string val = default;
+
+            linearSymbolTable.TryGet(2, out val);
+
+            Console.ReadKey();
+        }
+    }
+}
